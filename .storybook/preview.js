b4b496aae76c7f470b3adade8dec50303b031514
Dương Quang Vinh <duongquangvinh6@gmail.com>
2023-02-20 11:03:25 +0700

    feat: add BaseTemplate component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+// https://www.youtube.com/watch?v=Iu5aZDqZt8E&t=381s 53:41 / 1:32:35
---
+++ components/templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,10 @@
+import { IBaseTemplate } from './BaseTemplate';
+
+const base: IBaseTemplate = { sampleTextProp: 'sample Text Prop' };
+
+// const alt: IBaseTemplate = { sampleTextProp: 'Sample Text Prop Alt version' };
+
+export const mockBaseTemplateProps = {
+  base,
+  // alt
+};
