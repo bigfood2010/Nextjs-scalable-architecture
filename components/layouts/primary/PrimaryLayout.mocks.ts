9684b24bb9302e94cf98889ea71545feef59fa09
Dương Quang Vinh <duongquangvinh6@gmail.com>
2023-02-21 10:32:34 +0700

    feat: add Layout

---
+++ components/layouts/primary/PrimaryLayout.mocks.ts
@@ -0,0 +1,10 @@
+import { IPrimaryLayout } from './PrimaryLayout';
+
+const base: IPrimaryLayout = { sampleTextProp: 'sample Text Prop' };
+
+// const alt: IPrimaryLayout = { sampleTextProp: 'Sample Text Prop Alt version' };
+
+export const mockPrimaryLayoutProps = {
+  base,
+  // alt
+};
---
+++ components/layouts/sidebar/SidebarLayout.mocks.ts
@@ -0,0 +1,10 @@
+import { ISidebarLayout } from './SidebarLayout';
+
+const base: ISidebarLayout = { sampleTextProp: 'sample Text Prop' };
+
+// const alt: ISidebarLayout = { sampleTextProp: 'Sample Text Prop Alt version' };
+
+export const mockSidebarLayoutProps = {
+  base,
+  // alt
+};
